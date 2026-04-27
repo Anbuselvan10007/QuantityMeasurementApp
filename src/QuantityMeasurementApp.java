0c6f8f10f47e9a76d2011be382da7e027a441fad
Anbuselvan10007 <am9229@srmist.edu.in>
2026-04-27 14:49:10 +0530

    UC1: Feet measurement equality

--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -1,5 +1,22 @@
 public class QuantityMeasurementApp {
+
+    // UC1: Feet Measurement Equality
+    double value;
+
+    public QuantityMeasurementApp(double value) {
+        this.value = value;
+    }
+
+    public boolean equals(QuantityMeasurementApp other) {
+        return this.value == other.value;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Quantity Measurement App");
+        QuantityMeasurementApp feet1 = new QuantityMeasurementApp(3.0);
+        QuantityMeasurementApp feet2 = new QuantityMeasurementApp(3.0);
+        QuantityMeasurementApp feet3 = new QuantityMeasurementApp(5.0);
+
+        System.out.println("3 feet == 3 feet: " + feet1.equals(feet2)); // true
+        System.out.println("3 feet == 5 feet: " + feet1.equals(feet3)); // false
     }
 }
