ee9deb3e94f07fc7a3271fcc7e4f8d3c36aa47bd
Anbuselvan10007 <am9229@srmist.edu.in>
2026-04-27 14:56:59 +0530

    UC4: Extended unit support yards and centimeters

--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -1,11 +1,13 @@
 public class QuantityMeasurementApp {
 
-    // UC3: Generic Quantity class - DRY Principle
+    // UC4: Extended Unit Support - Yards and Centimeters added
     enum Unit {
-        FEET(1.0),
-        INCH(1.0 / 12.0);
+        FEET(12.0),
+        INCH(1.0),
+        YARD(36.0),
+        CENTIMETER(0.393701);
 
-        final double conversionFactor;
+        final double conversionFactor; // everything converts to inches as base
 
         Unit(double conversionFactor) {
             this.conversionFactor = conversionFactor;
@@ -15,44 +17,44 @@ public class QuantityMeasurementApp {
     double value;
     Unit unit;
 
-    // Single generic constructor for any unit
     public QuantityMeasurementApp(double value, Unit unit) {
         this.value = value;
         this.unit = unit;
     }
 
-    // Convert any unit to base unit (feet)
-    public double convertToFeet() {
+    // Convert all units to inches as base unit
+    public double convertToInches() {
         return this.value * this.unit.conversionFactor;
     }
 
-    // Single equals() for all units - no duplication
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
         if (obj == null || getClass() != obj.getClass()) return false;
         QuantityMeasurementApp other = (QuantityMeasurementApp) obj;
-        return Double.compare(this.convertToFeet(), other.convertToFeet()) == 0;
+        return Double.compare(this.convertToInches(), other.convertToInches()) == 0;
     }
 
     public static void main(String[] args) {
-        // Feet comparisons (UC1 still works)
+        // Feet comparisons
         QuantityMeasurementApp feet1 = new QuantityMeasurementApp(1.0, Unit.FEET);
         QuantityMeasurementApp feet2 = new QuantityMeasurementApp(1.0, Unit.FEET);
-        QuantityMeasurementApp feet3 = new QuantityMeasurementApp(2.0, Unit.FEET);
-        System.out.println("1 feet == 1 feet: " + feet1.equals(feet2)); // true
-        System.out.println("1 feet == 2 feet: " + feet1.equals(feet3)); // false
+        System.out.println("1 feet == 1 feet: " + feet1.equals(feet2));       // true
 
-        // Inches comparisons (UC2 still works)
+        // Inch comparisons
         QuantityMeasurementApp inch1 = new QuantityMeasurementApp(12.0, Unit.INCH);
-        QuantityMeasurementApp inch2 = new QuantityMeasurementApp(12.0, Unit.INCH);
-        QuantityMeasurementApp inch3 = new QuantityMeasurementApp(1.0, Unit.INCH);
-        System.out.println("12 inch == 12 inch: " + inch1.equals(inch2)); // true
-        System.out.println("12 inch == 1 inch:  " + inch1.equals(inch3)); // false
+        System.out.println("1 feet == 12 inch: " + feet1.equals(inch1));      // true
 
-        // Cross unit comparison
-        QuantityMeasurementApp oneFeet = new QuantityMeasurementApp(1.0, Unit.FEET);
-        QuantityMeasurementApp twelveInch = new QuantityMeasurementApp(12.0, Unit.INCH);
-        System.out.println("1 feet == 12 inch:  " + oneFeet.equals(twelveInch)); // true
+        // Yard comparisons
+        QuantityMeasurementApp yard1 = new QuantityMeasurementApp(1.0, Unit.YARD);
+        QuantityMeasurementApp feet3 = new QuantityMeasurementApp(3.0, Unit.FEET);
+        QuantityMeasurementApp inch2 = new QuantityMeasurementApp(36.0, Unit.INCH);
+        System.out.println("1 yard == 3 feet:  " + yard1.equals(feet3));      // true
+        System.out.println("1 yard == 36 inch: " + yard1.equals(inch2));      // true
+
+        // Centimeter comparisons
+        QuantityMeasurementApp cm1 = new QuantityMeasurementApp(2.54, Unit.CENTIMETER);
+        QuantityMeasurementApp inch3 = new QuantityMeasurementApp(1.0, Unit.INCH);
+        System.out.println("2.54 cm == 1 inch: " + cm1.equals(inch3));        // true
     }
 }
