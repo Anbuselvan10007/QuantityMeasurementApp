611ae05159489bd2895387bf4dbdcfc13803f40b
Anbuselvan10007 <am9229@srmist.edu.in>
2026-04-27 14:51:01 +0530

    UC2: Feet and inches measurement equality

--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -1,22 +1,39 @@
 public class QuantityMeasurementApp {
 
-    // UC1: Feet Measurement Equality
-    double value;
+    // UC2: Feet and Inches Measurement Equality
+    enum Unit {
+        FEET(12.0),
+        INCH(1.0);
 
-    public QuantityMeasurementApp(double value) {
+        double conversionFactor;
+
+        Unit(double conversionFactor) {
+            this.conversionFactor = conversionFactor;
+        }
+    }
+
+    double value;
+    Unit unit;
+
+    public QuantityMeasurementApp(double value, Unit unit) {
         this.value = value;
+        this.unit = unit;
+    }
+
+    public double convertToBaseUnit() {
+        return this.value * this.unit.conversionFactor;
     }
 
     public boolean equals(QuantityMeasurementApp other) {
-        return this.value == other.value;
+        return this.convertToBaseUnit() == other.convertToBaseUnit();
     }
 
     public static void main(String[] args) {
-        QuantityMeasurementApp feet1 = new QuantityMeasurementApp(3.0);
-        QuantityMeasurementApp feet2 = new QuantityMeasurementApp(3.0);
-        QuantityMeasurementApp feet3 = new QuantityMeasurementApp(5.0);
+        QuantityMeasurementApp feet1 = new QuantityMeasurementApp(1.0, Unit.FEET);
+        QuantityMeasurementApp inch1 = new QuantityMeasurementApp(12.0, Unit.INCH);
+        QuantityMeasurementApp inch2 = new QuantityMeasurementApp(1.0, Unit.INCH);
 
-        System.out.println("3 feet == 3 feet: " + feet1.equals(feet2)); // true
-        System.out.println("3 feet == 5 feet: " + feet1.equals(feet3)); // false
+        System.out.println("1 feet == 12 inch: " + feet1.equals(inch1)); // true
+        System.out.println("1 feet == 1 inch: " + feet1.equals(inch2));  // false
     }
 }
